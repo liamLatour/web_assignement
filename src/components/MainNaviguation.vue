931adc445140d927c5752eac5578ec995e6edb78
Liam LATOUR <liam.latour@gmail.com>
2022-11-14 04:35:47 +0100

    Changed routes

--- src/components/MainNaviguation.vue
+++ src/components/MainNaviguation.vue
@@ -1,10 +1,18 @@
 <template>
   <div class="bg-neutral-800">
     <ul class="flex m-auto max-w-5xl">
-      <a class="p-1 m-2 text-base rounded" href="/buildings"> Buildings </a>
-      <a class="p-1 m-2 text-base rounded" href="/rooms"> Rooms </a>
-      <a class="p-1 m-2 text-base rounded" href="/heaters"> Heaters </a>
-      <a class="p-1 m-2 text-base rounded" href="/windows">Windows</a>
+      <a class="p-1 m-2 text-base rounded" href="/web_assignement/buildings">
+        Buildings
+      </a>
+      <a class="p-1 m-2 text-base rounded" href="/web_assignement/rooms">
+        Rooms
+      </a>
+      <a class="p-1 m-2 text-base rounded" href="/web_assignement/heaters">
+        Heaters
+      </a>
+      <a class="p-1 m-2 text-base rounded" href="/web_assignement/windows">
+        Windows
+      </a>
     </ul>
   </div>
 </template>
--- vite.config.js
+++ vite.config.js
@@ -4,46 +4,22 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode, ssrBuild }) => {
-  if (command === "serve") {
-    return {
-      plugins: [vue()],
-      resolve: {
-        alias: {
-          "@": fileURLToPath(new URL("./src", import.meta.url)),
-        },
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        //target: "http://127.0.0.1:8080",
+        target: "http://liam-latour.cleverapps.io/",
+        changeOrigin: true,
+        secure: false,
       },
-      server: {
-        proxy: {
-          "/api": {
-            //target: "http://127.0.0.1:8080",
-            target: "http://liam-latour.cleverapps.io/",
-            changeOrigin: true,
-            secure: false,
-          },
-        },
-      },
-      base: "/",
-    };
-  } else {
-    return {
-      plugins: [vue()],
-      resolve: {
-        alias: {
-          "@": fileURLToPath(new URL("./src", import.meta.url)),
-        },
-      },
-      server: {
-        proxy: {
-          "/api": {
-            //target: "http://127.0.0.1:8080",
-            target: "http://liam-latour.cleverapps.io/",
-            changeOrigin: true,
-            secure: false,
-          },
-        },
-      },
-      base: "/web_assignement/",
-    };
-  }
+    },
+  },
+  base: "/web_assignement/",
 });
