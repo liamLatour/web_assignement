df85a078f3f009d65aab9b08d44b591c64d47920
Liam LATOUR <liam.latour@gmail.com>
2022-11-14 03:49:30 +0100

    Moved everything

--- frontend/src/views/HomeView.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <h1>Hey</h1>
-</template>
-
-<script></script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex flex-col m-auto justify-center items-center p-4 max-w-2xl">
+    <h1 class="text-4xl p-4 text-white">Welcome to Faircorp's frontend !</h1>
+    <p class="text-2xl p-4 mt-4">
+      Here you can administrate Faircorp's API in a user friendly way, start by selecting one of the tabs above.
+    </p>
+  </div>
+</template>
+
+<script></script>
